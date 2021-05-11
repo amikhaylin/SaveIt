9feea7bfca4dfd8bb9c08b91516680a108a701d3
Andrey Mikhaylin <cenobith@gmail.com>
2021-05-11 11:46:14 +0300

    Initial Commit

---
+++ SaveIt/SaveItApp.swift
@@ -0,0 +1,17 @@
+//
+//  SaveItApp.swift
+//  SaveIt
+//
+//  Created by Andrey Mikhaylin on 11.05.2021.
+//
+
+import SwiftUI
+
+@main
+struct SaveItApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
